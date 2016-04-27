76f28f683503a90adbd1b4f9ef097dc39e5cf0b7
davidmctavish <david.mctavish@utoronto.ca>
2016-04-26 21:50:27 -0400

    Read short reads from file.
    (Strange bug remaining: running twice with a long short read file causes program to freeze on reading reference sequence)

--- Source/PetaLinux/sequencer_v_0_2/components/apps/RunSequencer/RunSequencer.cpp
+++ Source/PetaLinux/sequencer_v_0_2/components/apps/RunSequencer/RunSequencer.cpp
@@ -26,6 +26,8 @@ int compare_seq(char* s1, char* s2, unsigned int seq_length);
 //copies sequence from origin to destination. 
 void copy_seq(char* origin, char* dest, unsigned int seq_length);
 
+void read_short_reads(char* sr_filename, char*** sr_array, unsigned int *num_reads);
+
 int main(int argc, char* argv[])
 {
 	long int val = 0;
@@ -118,7 +120,7 @@ int main(int argc, char* argv[])
 		t_sequence->seq_ptr[i] = rotations[i][t_sequence->seq_length - 1];
 	}
 	//print out original sequence
-	cout << "Original sequence" << endl;
+	cout << "Original sequence (" << sequence->seq_length << ")" << endl;
 	for (unsigned int i = 0; i < sequence->seq_length; i++){
 		cout << sequence->seq_ptr[i];
 	}
@@ -316,7 +318,7 @@ int main(int argc, char* argv[])
 
 	// NEW - RUN SEQUENCER
 	if (argc < 3) {
-		printf("Enter up to 3 short reads (all caps) as {2nd/3rd/4th} argument!\n");
+		printf("Enter a file name contatining the short reads as 2nd argument!\n");
 		return -1;
 	}
 
@@ -333,54 +335,84 @@ int main(int argc, char* argv[])
 
 	volatile unsigned int *sr_bram_ptr[NUM_SR_BRAMS] = { NULL };
 	unsigned int short_reads_to_run = 0;
-	if (argc >= 3) {
-		short_reads_to_run = 1;
+	char** short_read_data;
 
+	printf("Starting to read short reads\n");
+	read_short_reads(argv[2], &short_read_data, &short_reads_to_run);
+
+	for (unsigned int i = 0; i < short_reads_to_run; i++) {
+		printf("SR %d: %s\n", i, short_read_data[i]);
+	}
+
+	
+	if (short_reads_to_run >= 1) {
 		sr_bram_ptr[0] = get_sr_bram_ptr(0);
 		
 		int sr_size = 0;
 		do {
-			write_bram_value_char(sr_bram_ptr[0], sr_size, argv[2][sr_size]);
+			//write_bram_value_char(sr_bram_ptr[0], sr_size, short_read_data[0][sr_size]);
 			sr_size++;
-		} while (argv[2][sr_size] != 0);
+		} while (short_read_data[0][sr_size] != 0);
+
+		// NOTE: need to write short reads backwards
+		for (unsigned int i = 0; i < sr_size; i++) {
+			unsigned int index = (sr_size - 1) - i;
+			write_bram_value_char(sr_bram_ptr[0], i, short_read_data[0][index]);
+		}
 
 		set_SR_Addr(sequencer_ptr[0], XPAR_AXI_BRAM_CTRL_SR_0_S_AXI_BASEADDR);
 		set_SR_length(sequencer_ptr[0], sr_size);
 	}
-	if (argc >= 4) {
-		short_reads_to_run = 2;
-
+	if (short_reads_to_run >= 2) {
 		sr_bram_ptr[1] = get_sr_bram_ptr(1);
 		
 		int sr_size = 0;
 		do {
-			write_bram_value_char(sr_bram_ptr[1], sr_size, argv[3][sr_size]);
+			//write_bram_value_char(sr_bram_ptr[1], sr_size, short_read_data[1][sr_size]);
 			sr_size++;
-		} while (argv[3][sr_size] != 0);
+		} while (short_read_data[1][sr_size] != 0);
+
+		// NOTE: need to write short reads backwards
+		for (unsigned int i = 0; i < sr_size; i++) {
+			unsigned int index = (sr_size - 1) - i;
+			write_bram_value_char(sr_bram_ptr[1], i, short_read_data[1][index]);
+		}
 
 		set_SR_Addr(sequencer_ptr[1], XPAR_AXI_BRAM_CTRL_SR_1_S_AXI_BASEADDR);
 		set_SR_length(sequencer_ptr[1], sr_size);
 	}
-	if (argc >= 5) {
-		short_reads_to_run = 3;
-
+	if (short_reads_to_run >= 3) {
 		sr_bram_ptr[2] = get_sr_bram_ptr(2);
 		
 		int sr_size = 0;
 		do {
-			write_bram_value_char(sr_bram_ptr[2], sr_size, argv[4][sr_size]);
+			//write_bram_value_char(sr_bram_ptr[2], sr_size, short_read_data[2][sr_size]);
 			sr_size++;
-		} while (argv[4][sr_size] != 0);
+		} while (short_read_data[2][sr_size] != 0);
+
+		// NOTE: need to write short reads backwards
+		for (unsigned int i = 0; i < sr_size; i++) {
+			unsigned int index = (sr_size - 1) - i;
+			write_bram_value_char(sr_bram_ptr[2], i, short_read_data[2][index]);
+		}
 
 		set_SR_Addr(sequencer_ptr[2], XPAR_AXI_BRAM_CTRL_SR_2_S_AXI_BASEADDR);
 		set_SR_length(sequencer_ptr[2], sr_size);
 	}
 
-	for (unsigned int i = 0; i < short_reads_to_run; i++) {
+	
+	unsigned int num_sequencers_running = short_reads_to_run;
+	if (num_sequencers_running > 3) {
+		num_sequencers_running = 3;
+	}
+
+	// Run all 3 sequencers in parallel
+	for (unsigned int i = 0; i < num_sequencers_running; i++) {
 		run_sequencer(sequencer_ptr[i]);
 	}
 
-	for (unsigned int i = 0; i < short_reads_to_run; i++) {
+	// poll until all 3 are done
+	for (unsigned int i = 0; i < num_sequencers_running; i++) {
 		sequencer_check_match(sequencer_ptr[i]);
 	}
 
@@ -419,3 +451,49 @@ void copy_seq(char* origin, char* dest, unsigned int seq_length){
 		dest[i] = origin[i];
 	}
 }
+
+
+void read_short_reads(char* sr_filename, char*** sr_array, unsigned int *num_reads) {
+	std::ifstream input_file;
+
+	printf("Reading short reads from ");
+	printf(sr_filename);
+	printf("\n");
+
+	input_file.open(sr_filename);
+
+	unsigned int num_short_reads = 0;
+	input_file >> num_short_reads;
+
+	*num_reads = num_short_reads;
+
+	*sr_array = (char**) malloc(num_short_reads * sizeof(char*));
+
+	for (unsigned int i = 0; i < num_short_reads; i++) {
+		unsigned int sr_length = 0;
+		input_file >> sr_length;
+		
+		(*sr_array)[i] = (char*) malloc((sr_length+1) * sizeof(char));
+
+		for (int j = 0; j < sr_length; j++) {
+			input_file >> (*sr_array)[i][j];
+		}
+		(*sr_array)[i][sr_length] = '\0';
+
+		printf("SR %d: %d, %s\n", i, sr_length, (*sr_array)[i]);
+	}
+
+	input_file.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
